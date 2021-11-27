bf88faff18ab17d3eb35a0b7ca464afd14e72e1f
estefany-rm <estefany18.ramirez@gmail.com>
2021-11-27 22:13:56 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,6 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
